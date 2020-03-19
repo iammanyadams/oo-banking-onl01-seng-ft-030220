ccfc8f86b606b91bc9203f5840b31aebbd287060
Nathaniel Adam <iammanyadams@gmail.com>
2020-03-19 09:47:29 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
 
+attr_reader :name
+attr_accessor :balance, :status
+attr_accessor :valid, :withdraw, :deposit
+
+
+def initialize(name)
+  @name, @status, @balance =name, "open", 1000
+  #@status ="open"
+  #@balance =1000
+
+end
+
+def deposit(deposit)
+  self.balance += deposit
+end
+def withdraw(withdraw)
+  self.balance = (balance - withdraw)
+end
+def display_balance
+  "Your balance is $#{self.balance}."
+end
+def valid?
+  self.balance > 0 && self.status == "open"
+end
+def close_account
+  @status = "closed"
+end
+
+
 end
